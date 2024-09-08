4af9cd88c6861d781e2fe2d90df71c075520e272
davidburnazyan <david.burnazyan.96@gmail.com>
2024-09-08 12:57:08 +0400

    rebase comment 2

--- index.js
+++ index.js
@@ -8,6 +8,6 @@
  *  1 in main branch
  *
  *
- * merge example ( Make other branch from main do some change and do a few commits)
- *
+ *  rebase example ( Make other branch from main do some change and do a few commits)
+ *  1
 */
